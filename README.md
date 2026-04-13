00c9758202dea566c39663fbf37156cc3170a1ab
Kim Austria <kcaustria021@gmail.com>
2026-04-12 22:21:28 -0600

    Edited README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# 3d-recon-pipeline
-A 3D reconstruction pipeline with SfS and mesh stitching
+# Multi-view Shape-from-Silhouette Reconstruction
+
+This repository contains code used in completion of a class project that investigates
+two different SfS paradigms.
+
+To run the reconstruction, choose an object from ["cube", "ellipsoid", "torus", "bunny"], then the SfS mode from ["volumetric", "halfspace"].
+
+Then, simply run:
+```
+sfs/run_sfs.sh {object} {mode}
+```
+
+To visualize the reconstruction, simply run:
+```
+python3.xx viz.py {path}
+```
+where `path` is the path to the ply file you want to view.
--- viz.py
+++ viz.py
@@ -3,14 +3,17 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="Parser")
-    parser.add_argument("view", type=int, help="Where to view the point cloud from")
+    parser.add_argument("input_path", type=str, help="The path to the ply file to visualize")
 
     args = parser.parse_args()
     input_path = args.input_path
 
-    print(f"Reading point cloud data from {input_path}...")
-    pcd = o3d.io.read_point_cloud(input_path)
-    o3d.visualization.draw_geometries([pcd])
+    if not input_path.endswith(".ply"):
+        print("Please enter a valid ply file")
+    else:
+        print(f"Reading point cloud data from {input_path}...")
+        pcd = o3d.io.read_point_cloud(input_path)
+        o3d.visualization.draw_geometries([pcd])
 
 if __name__ == "__main__":
     main()
